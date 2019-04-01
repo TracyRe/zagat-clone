4b6d4041e35d8693679ea17d9aed7f9bd5790cb6
Tracy Reith <treith@treith.com>
2019-04-01 12:50:24 -0700

    Add a few cities with array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { City } from './models/city.model';
+
+
+
 
 
 @Component({
@@ -7,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Zagat';
+
+  masterCityList: City[] = [
+  new City (`Atlanta`),
+  new City (`Austin`),
+  new City (`Baltimore`),
+  new City (`Boston`)
+  ]
+  //
+  // masterCityList: City[] = [
+  // `Atlanta`, `Austin`, `Baltimore`,  `Boston`
+  // `Charleston`, `Charlotte`, `Chicago`, `Cincinnati`, `Cleveland`, `Columbus`, `Dallas`, `Denver`, `Detroit`, `Houston`, `Indianapolis`, `Kansas City`, `Las Vegas`, `Los Angeles`, `Miami`, `Milwaukee`, `Minneapolis`, `Nashville`, `New Orleans`, `New York City`, `Oakland/Berkeley`, `Orlando`, `Philadelphia`, `Phoenix / Scottsdale`, `Providence`, `Salt Lake City`, `San Antonio`, `San Diego`, `San Francisco`, `Savannah`, `St. Louis`, `Washington, DC`
+  // ]
 }
--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { City } from '../models/city.model';
 
 @Component({
   selector: 'app-cities',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
+
+
+@Input() childCityList: City[];
+
 
   constructor() { }
 
