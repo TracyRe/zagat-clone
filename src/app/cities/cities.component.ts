0d70c6a2c2a830b3b0961e7a460f57a160d5cd82
Tracy Reith <treith@treith.com>
2019-04-05 11:03:05 -0700

    Return city name from database

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 import { City } from '../models/city.model';
 import { CityService } from '../services/city.service';
@@ -10,16 +12,20 @@ import { CityService } from '../services/city.service';
   providers: [CityService]
 })
 export class CitiesComponent implements OnInit {
-
-
-@Input() childCityList: City[];
-
+  citys: FirebaseListObservable<any[]>;
 
   constructor(
+    private router: Router,
     private cityService: CityService
   ) { }
 
   ngOnInit() {
+    this.citys = this.cityService.getCitys();
   }
+
+
+  loadCity(clickedCity) {
+    this.router.navigate(['citys', clickedCity.$key]);
+  };
 
 }
