0c7d0f353e95454d1b259d158e162c1259581b19
Tracy Reith <treith@treith.com>
2019-04-04 22:06:07 -0700

    Model data for front page based on city selection

--- src/app/models/city.model.ts
+++ src/app/models/city.model.ts
@@ -1,5 +1,24 @@
 export class City {
   constructor (
-    public name: string
+    public name: string,
+    public bestBgImg: string,
+    public bestCitySymbol: string,
+    public latestCard1Img: string,
+    public latestCard1Title: string,
+    public latestCard1Date: string,
+    public latestCard2Img: string,
+    public latestCard2Title: string,
+    public latestCard2PlaceCount: string,
+    public latestCard3Img: string,
+    public latestCard3Title: string,
+    public latestCard3PlaceCount: string,
+    public latestCard4Img: string,
+    public latestCard4Title: string,
+    public latestCard5TitleImg: string,
+    public latestCard5Title: string,
+    public latestCard6Img: string,
+    public latestCard6Title: string,
+    public latestCard7Img: string,
+    public latestCard7Title: string,    
   ) {}
 }
